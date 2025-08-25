a533381eb25c2c389c94a6f4830aada50a8b8e4e
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-25 18:56:34 +0300

    added rentalapplication and leaseagreement serializers

--- rental/serializers.py
+++ rental/serializers.py
@@ -6,9 +6,24 @@ class ImagesSerializer(serializers.ModelSerializer):
         model = Images
         fields = ['images',]
         
+
 class RentalHouseSerializer(serializers.ModelSerializer):
     images = ImagesSerializer(many=True, read_only=True)
     class Meta:
         model = RentalHouse
         fields = ['__all__']
-        
+class RentalApplicationSerializer(serializers.ModelSerializer):      
+    house_id = serializers.CharField(source='house.id')
+    tenant_name = serializers.CharField(source='tenant.username', read_only=True)
+    class Meta:
+        model = RentalApplication
+        fields = ['__all__']
+
+class LeaseAgreementSerializer(serializers.ModelSerializer):
+    application_id = serializers.CharField(source='application.id')
+    tenant_name = serializers.CharField(source='tenant_user.username', read_only=True)
+    landlord_name = serializers.CharField(source='landlord_user.username', read_only=True)
+
+    class Meta:
+        model = LeaseAgreement
+        fields = ['__all__']
