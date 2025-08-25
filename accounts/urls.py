54790532ba3d74f01e340b0c2b87b2638ae43edb
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-25 11:40:31 +0300

    split my accounts url

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,31 +7,38 @@ from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 
 router = DefaultRouter()
-router.register(r'api/users', views.UserViewSet)
-router.register(r'api/profiles', views.ProfileViewSet)
+router.register(r'users', views.UserViewSet, basename='user')
+router.register(r'profiles', views.ProfileViewSet, basename='profile')
+app_name = 'accounts'
+
 """
-DRF enpoints for the router
 Normal django urls
+DRF enpoints for the router
 authentication views for the login and logging out
 DRf api urls
 """
 
 
-urlpatterns = [
+traditional_urlpatterns = [
+    # traditional
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='accounts/login.html', success_url=reverse_lazy('homepage')), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
     path('dashboard/', views.user_profile_view, name='user_profile_dashboard'),
     path('profile/', views.user_profile_view, name='user_profile_view'), 
     path('profile/edit/', views.user_profile_edit, name='user_profile_edit'),
     path('profile/upgrade-role/', views.upgrade_role, name='upgrade_role'),
-
+]
+api_urlpatterns = [
+    # api
+    path('register/', views.RegistrationAPIView.as_view(), name='api_register'),
+    path('upgrade-role/', views.RoleUpgradeAPIView.as_view(), name='api_upgrade_role'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)), 
-    path('api/upgrade-role/', views.RoleUpgradeAPIView.as_view(), name='api_upgrade_role'),
-    path('api/register/', views.RegistrationAPIView.as_view(), name='api_register'),
-    
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
+urlpatterns = [
+    path('', include(traditional_urlpatterns)),
+    path('api/', include(api_urlpatterns)),
+]
