d1b946fc848ac04080e926ed2ce11f21a0796d1f
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-26 11:38:19 +0300

    configured both my traditional and api urls

---
+++ rental/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from .import views
+
+router = DefaultRouter
+router.register(r'houses', views.RentalHouseViewSet, basename='rental_house')
+router.register(r'applications', views.RentalApplicationViewSet, basename='rental_application')
+router.register(r'applications', views.LeaseAgreementViewSet, basename='lease_agreement')
+app_name = 'rental'
+
+urlpatterns = [
+    # T-urls
+    path('', views.rental_properties_list, name='rental_properties_list'),
+    path('house/', views.rental_property_detail, name='rental_property_detail'),
+    path('my-applications/', views.my_applications, name='my_applications'),
+    
+    # API urls
+    path('api/', include(router.urls)),
+]
+
