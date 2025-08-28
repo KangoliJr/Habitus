268f270f6c29d4fef23df5734cc48d8e796dd91f
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-28 22:05:45 +0300

    added validation for booking in the airbnb

--- airbnb/serializers.py
+++ airbnb/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import AirbnbHouse, Booking,Images
+from django.core.exceptions import ValidationError
+
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,8 +15,26 @@ class BookingSerializer(serializers.ModelSerializer):
     house = serializers.CharField(source='house.name', read_only=True)
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = [
+            'id', 'house', 'checkin', 'checkout', 'status', 'created_at',
+            'customer_username', 'house_name'
+        ]
         read_only_fields = ['id', 'customer', 'house', 'status''created_at']
+        
+    def validate(self, data):
+        checkin_date = data.get('checkin')
+        checkout_date = data.get('checkout')
+        house = data.get('house')
+        
+        # Check for booking conflicts
+        if Booking.objects.filter(
+            house=house,
+            checkout__gt=checkin_date,
+            checkin__lt=checkout_date
+        ).exists():
+            raise serializers.ValidationError("This house is already booked for the selected dates.")
+        
+        return data
 
 class AirbnbHouseSerializer(serializers.ModelSerializer):
     images = ImageSerializer(many=True, read_only=True)
@@ -23,8 +43,8 @@ class AirbnbHouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = AirbnbHouse
         fields = [
-            'name','host', 'description', 'price', 'furnishing_style',
+            'id','name','host', 'description', 'price', 'furnishing_style',
             'bedroom', 'bathroom', 'location', 'rules', 'amenities',
             'images', 'bookings'
         ]
-        read_only_fields = ['host']
+ 
