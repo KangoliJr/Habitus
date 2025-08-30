ad274f6718d8191dace5508e72c85b2b01e36ade
Joshua Kangoli <joshua.kang'oli@students.uonbi.ac.ke>
2025-08-30 21:49:38 +0300

    added nested urls for applications and images to pull a house unit together

--- rental/urls.py
+++ rental/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path,include
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from .import views
 
 router = DefaultRouter()
 router.register(r'houses', views.RentalHouseViewSet, basename='rental_house')
-router.register(r'images', views.ImagesViewSet, basename='images')
+router.register(r'applications', views.RentalApplicationViewSet, basename='rental-applications')
+router.register(r'agreements', views.LeaseAgreementViewSet, basename='lease-agreements')
+
+houses_router = routers.NestedDefaultRouter(router, r'houses', lookup='house')
+houses_router.register(r'images', views.ImagesViewSet, basename='house-images')
+houses_router.register(r'applications', views.RentalApplicationViewSet, basename='house-applications')
+
+applications_router = routers.NestedDefaultRouter(router, r'applications', lookup='application')
+applications_router.register(r'agreements', views.LeaseAgreementViewSet, basename='application-agreements')
 app_name = 'rental'
 
 urlpatterns = [
@@ -20,5 +29,7 @@ urlpatterns = [
     
     # API urls
     path('api/', include(router.urls)),
+    path('api/', include(houses_router.urls)),
+    path('api/', include(applications_router.urls)),
 ]
 
